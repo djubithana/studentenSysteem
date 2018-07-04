4fdf9749086de95d76f438520b152fc169eacabf
dencioSVN <dencioSVN@2ee4400e-80d5-4b90-9877-2e62fd6be0a4>
2018-07-04 22:18:20 +0000

    entities geupdate

---
+++ src/main/java/sr/havo1/webapp/studentenvolgsysteem/dao/RollenRechtenDAO.java
@@ -0,0 +1,74 @@
+package sr.havo1.webapp.studentenvolgsysteem.dao;
+
+import sr.havo1.webapp.studentenvolgsysteem.entity.RollenRechten;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by DENCIO on 6/19/2018.
+ */
+public class RollenRechtenDAO {
+
+    public static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("studentenvolgsysteem");
+
+    public RollenRechten addRolRecht(RollenRechten rolRecht) {
+        // Create an EntityManager
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+
+        RollenRechten newRolRecht = new RollenRechten();
+        try {
+            // Get a transaction
+            transaction = manager.getTransaction();
+            // Begin the transaction
+            transaction.begin();
+
+            // Create a new object
+            newRolRecht.setRol_id(rolRecht.getRol_id());
+            newRolRecht.setRecht_id(rolRecht.getRecht_id());
+
+            // Save the object
+            manager.persist(newRolRecht);
+
+            // Commit the transaction
+            transaction.commit();
+        } catch (Exception ex) {
+            // If there are any exceptions, roll back the changes
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            // Print the Exception
+            ex.printStackTrace();
+        } finally {
+            // Close the EntityManager
+            manager.close();
+        }
+        return newRolRecht;
+    }
+
+    public RollenRechten getRolRecht(Long rol_recht_id) {
+        RollenRechten rolRecht = new RollenRechten();
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            transaction = manager.getTransaction();
+            transaction.begin();
+            rolRecht = manager.find(RollenRechten.class, rol_recht_id);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        } finally {
+            manager.close();
+        }
+        return rolRecht;
+    }
+}
