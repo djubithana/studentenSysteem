989781548990e7720abc02e3208f12a8c17df932
djubithana <dwightjubi@gmail.com>
2018-07-06 02:01:04 -0300

    User Interface en extra paginas

--- src/main/webapp/inc/sidebar.php
+++ src/main/webapp/inc/sidebar.php
@@ -1,12 +1,12 @@
 <div class="sidebar col-md-2">
 		<img class="logo_login" src="img/havo.png" alt="havo logo" width="150px"/>
 		<ul>
-			<li><a href="dashboard.php">Dashboard</a></li>
-			<li><a href="schooljaar.php">Schooljaar</a></li>
-			<li><a href="">Klassen Overzicht</a></li>
-			<li><a href="">Vakken Overzicht</a></li>
-			<li><a href="">Rechten en Rollen</a></li>
-			<li><a href="">Gebruikers</a></li>
-			<li><a href="">Account</a></li>
+			<li><a href="dashboard.php"><i class="fas fa-tachometer-alt fa-1x menu-icon"></i>Dashboard</a></li>
+			<li><a href=""><i class="fas fa-user fa-1x menu-icon"></i>Profiel</a></li>
+			<li><a href="schooljaar.php"><i class="fas fa-calendar-alt fa-1x menu-icon"></i>Schooljaar</a></li>
+			<li><a href=""><i class="fas fa-book fa-1x menu-icon"></i>Klassen</a></li>
+			<li ><a href=""><i class="fas fa-book-open fa-1x menu-icon" ></i>Vakken</a><span class="caret"></span></li>
+			<li><a href="gebruikers.php"><i class="fas fa-users fa-1x menu-icon"></i>Gebruikers</a></li>
+			<li><a href=""><i class="fas fa-sign-out-alt fa-1x menu-icon"></i>Uitloggen</a></li>
 		</ul>
 </div>
