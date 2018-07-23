cedf4b4f637a9cbc18cdd6b5ff4c212aa50d2649
Cherelys <Cherelys_cher@hotmail.com>
2018-07-23 11:13:39 -0300

    entities geupdate

--- src/main/webapp/uitloggen.php
+++ src/main/webapp/uitloggen.php
@@ -6,7 +6,7 @@
                                  			<section class="jumbotron text-center header-jumbo">
                                  				<div class="container">
                                  					<h4 class="jumbotron-heading">Pagina Titel Komt hier</h4>
-                                 					<button class="btn_dark_bg">Toevoegen</button>
+
                                  				</div>
                                  			</section>
                                  			<div class="content">
